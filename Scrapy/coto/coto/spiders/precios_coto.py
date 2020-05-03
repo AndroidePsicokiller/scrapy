9157c6c3a4fc130df34d75bd40793f85e2be17b4
AndroidePsicokiller <49503878+AndroidePsicokiller@users.noreply.github.com>
2020-05-03 17:54:32 +0200

    Add files via upload

--- Scrapy/coto/coto/spiders/precios_coto.py
+++ Scrapy/coto/coto/spiders/precios_coto.py
@@ -5,22 +5,24 @@ import scrapy
 class PreciosCotoSpider(scrapy.Spider):
     name = 'precios_coto'
     allowed_domains = ['cotodigital3.com.ar']
-    start_urls = ['https://cotodigital3.com.ar/sitios/cdigi/browse/categoria-ofertas/', 'https://cotodigital3.com.ar/sitios/cdigi/']
+    start_urls = ['https://cotodigital3.com.ar/sitios/cdigi/browse/categoria-ofertas/']
 
     def parse(self, response):
-
+        self.it = 0
         productos = response.xpath('//*[@class="product_info_container"]/a/@href').extract()
         for url in productos:
             yield {'Product Url':url}
 
-        next_page_url = response.xpath('//*[@class="atg_store_pagination"]//*/a/@href').extract_first()
+        #next_page_url = response.xpath('//*[@class="atg_store_pagination"]//*/a/@href').extract()
+        next_page_url = response.xpath('//*[@class="atg_store_pagination"]//*/a/@href').extract()[len(response.xpath('//*[@class="atg_store_pagination"]//*/a/@href').extract())-1]
 
         if next_page_url is not None:
             absolute_next_page_url = 'https://www.cotodigital3.com.ar' + next_page_url
             print(absolute_next_page_url)
             next_page = response.urljoin(absolute_next_page_url)
-
-            yield scrapy.Request(next_page, callback=self.parse)
+            self.it += 1
+            if self.it < 10:
+                yield scrapy.Request(next_page, callback=self.parse)asd
 
 
 
